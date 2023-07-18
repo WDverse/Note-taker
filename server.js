749aa956f9cdc3fef8c2f170b5a09e8da2bed995
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-07-18 23:52:31 +0000

    formated server.js

--- server.js
+++ server.js
@@ -12,49 +12,49 @@ app.use(express.static('public'));
 
 const { readFromFile, readAndAppend } = require('./helpers/fsUtilis');
 
-app.get('/notes',(req, res)=> {
-  res.sendFile(path.join(__dirname,'./public/notes.html'));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 })
-app.get('*',(req, res)=> {
-  res.sendFile(path.join(__dirname,'./public/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 
 app.get('/', (req, res) => {
-    console.info(`${req.method} request received for notes`);
-  
-    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
-  });
+  console.info(`${req.method} request received for notes`);
+
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
 
 
-  app.post('/', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to submit notes`);
-  
-    // Destructuring assignment for the items in req.body
-    const {noteTitile, noteText } = req.body;
-  
-    // If all the required properties are present
-    if (noteTitile && noteText) {
-      // Variable for the object we will save
-      const newNote = {
-        noteTitile,
-        noteText,
-        notes_id: uuid(),
-      };
-  
-      readAndAppend(newNote, './db/notes.json');
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      res.json(response);
-    } else {
-      res.json('Error in saving notes');
-    }
-  });
+app.post('/', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to submit notes`);
+
+  // Destructuring assignment for the items in req.body
+  const { noteTitile, noteText } = req.body;
+
+  // If all the required properties are present
+  if (noteTitile && noteText) {
+    // Variable for the object we will save
+    const newNote = {
+      noteTitile,
+      noteText,
+      notes_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/notes.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in saving notes');
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
